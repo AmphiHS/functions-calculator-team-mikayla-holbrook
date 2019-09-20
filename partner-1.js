6a6149a6aeed76d44a462efe67886f5449805fb3
ahsmikayla1234 <mikaylanne2001@gmail.com>
2019-09-20 19:40:04 +0000

    added miles to kilometers function

--- partner-1.js
+++ partner-1.js
@@ -1,6 +1,15 @@
 //Name: Mikayla Holbrook
+//notetoself dont forget to commit changes every time you add a function
 
 
 const feetToMeters = (feet) => {
     return feet * 0.305;
+}
+
+const poundsToKilograms = (pounds) => {
+    return pounds * 0.45;
+}
+
+const milesToKilometers = (miles) => {
+    return miles * 1.609;
 }
